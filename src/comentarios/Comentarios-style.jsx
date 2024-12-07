f674ca1a17c0c646b2f9a40f76ec5d655151782c
Nicolas Stigler <nicolas.stigler@utec.edu.pe>
2024-12-07 01:08:31 -0500

    comentarios

---
+++ src/comentarios/Comentarios-style.jsx
@@ -0,0 +1,44 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+max-width: 1200px;
+margin: 0 auto;
+padding: 2rem;
+`;
+
+export const Title = styled.h1`
+color: #231123;
+margin-bottom: 2rem;
+font-size: 2rem;
+`;
+
+export const ReviewsContainer = styled.div`
+background-color: #fff;
+border-radius: 10px;
+box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+padding: 2rem;
+`;
+
+export const ReviewsTable = styled.table`
+width: 100%;
+border-collapse: collapse;
+margin-top: 2rem;
+`;
+
+export const TableRow = styled.tr`
+&:nth-child(even) {
+    background-color: #f9f9f9;
+}
+`;
+
+export const TableHeader = styled.th`
+text-align: left;
+padding: 1rem;
+background-color: #82204a;
+color: #fff;
+`;
+
+export const TableCell = styled.td`
+padding: 0.75rem;
+border-bottom: 1px solid #ddd;
+`;
